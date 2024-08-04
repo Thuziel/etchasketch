284808cfe6b5633ab3fec393286ec686901ac781
Thuziel <48653177+Thuziel@users.noreply.github.com>
2024-08-04 15:27:11 +0300

    Set up grid generation
    - Set up html input and js ogic for setting grid size
    - Set up html input and js logic for changing grid square sizes
    - Set up flex css to keep grid a grid

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+const root = document.documentElement;
 const container = document.querySelector('#container');
-for(let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    div.classList.add('square');
-    container.appendChild(div);
+
+function deleteGrid() {
+    const tempContainer = document.querySelector('#tempContainer');
+    container.removeChild(tempContainer);
+
 }
+
+function generateGrid(size) {
+    const tempContrainer = document.createElement('div');
+    tempContrainer.id = "tempContainer";
+    for(let ii = 0; ii < size; ii++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for(let i = 0; i < size; i++) {
+            const div = document.createElement('div');
+            div.classList.add('square');
+            row.appendChild(div);
+        }
+        tempContrainer.append(row);
+    }
+    container.appendChild(tempContrainer);
+}
+
+function resizeSquares(size) {
+    root.style.setProperty("--square-size", size+"px");
+}
+
+const gridSizeInput = document.querySelector('#gridSizeInput');
+gridSizeInput.addEventListener('keyup', (e) => {
+    deleteGrid();
+    generateGrid(e.target.value);
+});
+
+const squareSizeInput = document.querySelector('#squareSizeInput');
+squareSizeInput.addEventListener('keyup', (e) => {
+    resizeSquares(e.target.value);
+})
+
+generateGrid(4);
